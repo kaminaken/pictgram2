83735933fd44dc6019146a4e6251ec5304783fb8
kaminaken <cubethe27@gmail.com>
2020-11-29 12:04:04 +0900

    パスワード複雑化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A\S+@\S+\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)\S+\z/
+  validates :name, presence: true, length: {maximum:15}
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: {minimum:8, maximum:32}, format: { with: VALID_PASSWORD_REGEX }
 
   has_secure_password
 end
